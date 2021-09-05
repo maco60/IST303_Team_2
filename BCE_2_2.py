82a2b33b70bcd427f30b84b454d42fba3595ef2c
Mike <maco70090@gmail.com>
2021-09-05 15:07:34 -0700

    Update BCE_2_2.py

--- BCE_2_2.py
+++ BCE_2_2.py
@@ -1,2 +1,33 @@
-print("Hello World!")
-print("Cats")
+#Task 1 Answer:
+vb = 1000000
+usd = vb*0.016
+
+print(vb, 'Venezuelan Bolivars')
+print(usd, 'US dollars')
+
+#Task 2 Answer:
+yusd = 12060
+yvb = 12060/0.016
+
+print('You need to have', yvb*3, 'Venezuelan Bolivars', 'to live for the next 3 years.')
+
+#Task 3 Answer:
+ym = 365.2425*24*60
+print('You would spend', yvb/ym, 'Venezuelan Bolivars per minute in the next 3 years')
+
+#Task 4 Answer:
+bloody_vikings = 'Wonderful Spam! Glorious Spam!'
+
+#Task 5 Answer:
+viking_exclamations = bloody_vikings.split("! ", 1)
+
+#Task 6 Answer:
+viking_exaggeration = viking_exclamations[1]
+
+#Task 7 Answer:
+upper_exaggeration = viking_exaggeration.upper()
+print(upper_exaggeration)
+
+#Task 8 Answer:
+partial_exaggeration = viking_exaggeration[3:7]
+print(partial_exaggeration)
